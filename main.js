ee8faafb3ea8ad54758309c4de7227eb76124892
Amine derbal <aminedoran31@gmail.com>
2024-11-16 20:24:10 +0100

    Add initialization

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 import './style.css';
+import { postApiRequest, generatePostData } from './src/postApiRequest.js';
+import { fetchApiRequest } from './src/fetchApiRequest.js';
+import { generateUpdateData } from './src/updateApiRequests.js';
+import { setupCustomFields } from './src/handleForm.js';
+
+window.onload = async () => {
+  const customFieldsContainer = document.getElementById('customFields');
+  const addCustomFieldButton = document.getElementById('addCustomField');
+
+  const updateButton = document.getElementById('updatePostButton');
+
+  const updateFields = ['id', 'userId', 'title', 'body'];
+
+  const postData = generatePostData();
+  await postApiRequest(postData);
+  const fetchData = await fetchApiRequest();
+  console.log('response data from fetch request', fetchData);
+
+  setupCustomFields(customFieldsContainer, addCustomFieldButton, updateFields);
+  generateUpdateData(updateButton, updateFields);
+};
